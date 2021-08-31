2ec30a54e0851fa7666945c8a581f0cddf5056f7
omkar-pd <omkarpd109@gmail.com>
2021-08-31 13:06:22 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const deadline = new Date("17 Nov, 2021 00:00:00");
+const daysHtml = document.querySelector(".days");
+const hoursHtml = document.querySelector(".hours");
+const minutesHtml = document.querySelector(".minutes");
+const secondsHtml = document.querySelector(".seconds");
+const c = document.getElementById("wrapper");
+const interval = setInterval(function updateDate() {
+  const now = new Date();
+  const total = deadline - now;
+  const days = Math.floor(total / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((total % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((total % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((total % (1000 * 60)) / 1000);
+
+  if (total <= 0) {
+    clearInterval(interval);
+  } else {
+    daysHtml.innerText = days;
+    hoursHtml.textContent = hours;
+    minutesHtml.textContent = minutes;
+    secondsHtml.textContent = seconds;
+  }
+}, 1000);
